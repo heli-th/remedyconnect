5c85aed8d6d8e1cae46adec8e3cbfd1857d4c545
Heli <heli.t@covrize.com>
2025-09-18 13:57:05 +0530

    allow duda origin

--- src/middlewares/allowedOriginMiddleware.js
+++ src/middlewares/allowedOriginMiddleware.js
@@ -9,8 +9,34 @@ const checkAllowedOrigin = async (req, res, next) => {
   const { base: baseBody } = req.body;
   let hostname;
 
+  try {
+    hostname = new URL(origin).hostname;
+  } catch {
+    hostname = origin;
+  }
+
+  // handle CORS preflight
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
   if (req.headers["x-airtable-source"]) {
     return next();
+  }
+
+  if (
+    req.headers["x-site-deployment-id"] &&
+    req.headers["x-site-deployment-id"] === "dudacollection"
+  ) {
+    if (
+      req.headers["authorization"] &&
+      req.headers["authorization"].startsWith("Bearer ")
+    ) {
+      const token = req.headers["authorization"].split(" ")[1];
+      if (token === process.env.AIRTABLE_API_KEY) {
+        return next();
+      }
+    }
   }
 
   if (
@@ -24,17 +50,6 @@ const checkAllowedOrigin = async (req, res, next) => {
     return res
       .status(400)
       .json({ message: "base or baseId query parameter is required" });
-  }
-
-  try {
-    hostname = new URL(origin).hostname;
-  } catch {
-    hostname = origin;
-  }
-
-  // handle CORS preflight
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
   }
 
   try {
@@ -77,6 +92,21 @@ const checkAllowedOriginForGlobalBase = async (req, res, next) => {
     req.headers["x-postman-token"] === process.env.POSTMAN_SECRET
   ) {
     return next();
+  }
+
+  if (
+    req.headers["x-site-deployment-id"] &&
+    req.headers["x-site-deployment-id"] === "dudacollection"
+  ) {
+    if (
+      req.headers["authorization"] &&
+      req.headers["authorization"].startsWith("Bearer ")
+    ) {
+      const token = req.headers["authorization"].split(" ")[1];
+      if (token === process.env.AIRTABLE_API_KEY) {
+        return next();
+      }
+    }
   }
 
   if (req.headers["x-airtable-source"]) {
